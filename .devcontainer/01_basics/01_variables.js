bf7ce5391acc99220f529d6ef4b691c4af8fbdd9
monikashyam <monikasgaur@gmail.com>
2025-04-16 15:54:29 +0000

    dev

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =312620
+let accountEmail = "monu@google.com"
+var accountPassword ="12345"
+accountCity ="ghaziabad"
+
+//accountId = 2//
+
+accountEmail="hc@mo.com"
+accountPassword="222221"
+accountCity="shamli"
+
+console.log(accountId); 
+                               
+console.table([accountId,accountEmail,accountPassword,accountCity])
